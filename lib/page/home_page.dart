3baa9d29df189a822f79a7385447295c5ca63baf
LittleFish-Coder <adrianyu890317@gmail.com>
2023-01-30 23:46:35 +0800

    hightlight text when mouse leave

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,6 +1,3 @@
-import 'dart:html';
-
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ner_annotator/component/tag.dart';
